bb6616f56c5b1132267b7fea9793112919e8d0ba
Marcel de Menezes Araujo <araujomarcel89@gmail.com>
2024-05-16 16:37:25 -0300

    DASH ATUALIZACAO COM CRUD IMPLEMENTADO

--- Shop/Aplicativo/urls.py
+++ Shop/Aplicativo/urls.py
@@ -9,9 +9,16 @@ urlpatterns = [
     path('login', views.login, name='login'),
     path('cadastroVet/', views.cadastroVet, name='cadastroVet'),
     path('atualizacaoVet/', views.atualizacaoVet, name='atualizacaoVet'),
+    path('atualizacaoTutor/', views.atualizacaoTutor, name='atualizacaoTutor'),
+    path('updateTutor/<int:id>/', views.updateTutor, name='updateTutor'),
+    path('atualizacaoAnimal/', views.atualizacaoAnimal, name='atualizacaoAnimal'),
+    path('updateAnimal/<int:id>/', views.updateAnimal, name='updateAnimal'),
+    path('deleteTutor/<int:id>', views.deleteTutor, name='deleteTutor'),
+    path('deleteAnimal/<int:id>', views.deleteAnimal, name='deleteAnimal'),
     path('updateVet/<int:id>/', views.updateVet, name='updateVet'),
     path('deleteVet/<int:id>', views.deleteVet, name='deleteVet'),
     path('cadastroTutor/', views.cadastroTutor, name='cadastroTutor'),
     path('cadastroAnimal/', views.cadastroAnimal, name='cadastroAnimal'),
-    path('criar_agendamento/', views.criar_agendamento, name='criar_agendamento')
+    path('criar_agendamento/', views.criar_agendamento, name='criar_agendamento'),
+    path('dashatualizacao/', views.dashatualizacao, name='dashatualizacao')
 ]
